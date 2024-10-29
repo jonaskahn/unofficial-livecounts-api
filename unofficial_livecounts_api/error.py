cc1444a90afc2dbb57dcb68cf5ab3b98d6d2ae69
Jonas Kahn <tuyendev@gmail.com>
2024-10-30 01:17:49 +0700

    remove wrong annotation

--- unofficial_livecounts_api/error.py
+++ unofficial_livecounts_api/error.py
@@ -1,24 +1,21 @@
-from black.mode import Deprecated
-
-
 class RequestApiError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
 
 
-@Deprecated
+@DeprecationWarning
 class TiktokError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
 
 
-@Deprecated
+@DeprecationWarning
 class YoutubeError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
 
 
-@Deprecated
+@DeprecationWarning
 class TwitterError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
